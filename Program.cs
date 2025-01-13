5b28d5d599c184d75679c43b26e201405d349e27
selmalee <may.air@qq.com>
2025-01-14 00:44:30 +1300

    chore: movies

--- Program.cs
+++ Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
+// The using statement ensures that the database context is disposed after the seeding operation completes.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
